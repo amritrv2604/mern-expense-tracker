61ced972f7ecb041389a84b4d553ed6ba868644f
Amrit Raj Verma <amritverma7890@gmail.com>
2026-02-07 13:08:16 +0530

    Update login API endpoint to production URL

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -23,7 +23,7 @@ function Login() {
     try {
       setLoading(true);
 
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://mern-expense-tracker-cw9e.onrender.com/api/auth/login", {
         email,
         password,
       });
